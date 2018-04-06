60e1d802e41b8ad37004d21c996ddef7b95f54e5
Ildikó Tóth <ildiko@ildikototh.com>
2018-04-06 12:52:33 -0700

    completed checkpoint 11.

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,6 +13,20 @@
     changeSongs('previous');
   });
 
+  $('#time-control input').on('input', function(event){
+      player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState != 'playing'){ return; }
+
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime/duration)*100;
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+
+  }, 1000);
 
   function changeSongs(direction){
     if(player.playState != 'playing'){return;}
@@ -26,5 +40,5 @@
       player.playPause(album.songs[currentSongIndex-1]);
     }
   }
-  
+
 }
