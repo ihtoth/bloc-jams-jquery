4b97eff8c02b85b66e68c776c24f09374a63afb8
Ildikó Tóth <ildiko@ildikototh.com>
2018-04-06 11:38:53 -0700

    completed assignment 10. Previous and Next buttons on the player bar.

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,21 +1,30 @@
 {
+
   $('button#play-pause').on('click',function(){
     player.playPause();
     $(this).attr('playState',player.playState);
   });
 
-  $('button#next').on('click', function(){
+  $('button#next').on('click',function(){
+    changeSongs('next');
+  });
+
+  $('button#previous').on('click', function(){
+    changeSongs('previous');
+  });
+
+
+  function changeSongs(direction){
     if(player.playState != 'playing'){return;}
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex+1;
 
-    if(nextSongIndex >= album.songs.length){ return;}
-
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-
-
-  });
-
+    if (direction == 'next' && currentSongIndex+1 < album.songs.length){
+      player.playPause(album.songs[currentSongIndex+1]);
+    }
+    if (direction == 'previous' && currentSongIndex-1 >= 0){
+      player.playPause(album.songs[currentSongIndex-1]);
+    }
+  }
+  
 }
